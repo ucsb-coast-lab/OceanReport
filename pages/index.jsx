8f044c18b091e16651f3af1e995ac3d0450dd733
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-06-22 16:17:56 -0700

    sv -created basic layout

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Report from "../components/report.jsx";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>UCSB Ocean Report</h1>
+      <Report />
+    </div>
+  );
+}
