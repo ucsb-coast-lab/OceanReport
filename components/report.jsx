01616c88ed84bcfb65d0df1ad99604796242c92e
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-06-22 16:31:48 -0700

    sv -added base information and refresh button

--- components/report.jsx
+++ components/report.jsx
@@ -5,24 +5,20 @@ const linkDivStyle = {
 };
 
 export default function Report() {
-  const [{ date, wave, wind, tide, hi, lo }, setData] = useState({
-    date: "",
-    wave: "",
-    wind: "",
-    tide: "",
-    hi: "",
-    lo: "",
-  });
+  const setup = () => {
+    let date = "Monday, June 22 at 12:34 PM";
+    let wave = "2.3 ft @ 7 secs from 253º";
+    let wind = "Calm";
+    let tide = "3.6 ft and falling";
+    let hi = "2.6 ft @ 04:24 PM";
+    let lo = "6.2 ft @ 10:42 PM";
+    return { date, wave, wind, tide, hi, lo };
+  };
+
+  const [{ date, wave, wind, tide, hi, lo }, setData] = useState(setup());
 
   const update = () => {
-    setData({
-      date: "today",
-      wave: "3ft",
-      wind: "4knots",
-      tide: "2ft and rising",
-      hi: "5.7ft",
-      lo: "1.2ft",
-    });
+    setData(setup());
   };
 
   return (
@@ -31,8 +27,8 @@ export default function Report() {
       <h2>Wave height and period: {wave}</h2>
       <h2>Wind speed and direction: {wind}</h2>
       <h2>Tide: {tide}</h2>
-      <h2>Latest High Tide: {hi}</h2>
-      <h2>Latest Low Tide: {lo}</h2>
+      <h2>Next High Tide: {hi}</h2>
+      <h2>Next Low Tide: {lo}</h2>
       <a href="coastlab.sofar.com">More Wave History Here</a>
       <button onClick={update}>Refreash Report</button>
     </div>
