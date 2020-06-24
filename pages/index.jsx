3bbfa9be9f8228f11b19e61b176aca8659dcd647
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-06-24 12:12:25 -0700

    sv styled the page like the original website

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,13 +6,54 @@ export default function HomePage() {
   return (
     <div className={styles.page}>
       <div className={styles.navbar}>
-        <h1 className={styles.title}>UCSB Ocean Report</h1>
-        <div className={styles.homeButton}>
-          <a className={styles.buttons} href="https://ucsbcoastlab.org/">
-            Home
+        <a className={styles.navbarTitle} href="https://ucsbcoastlab.org/">
+          UCSB COAST Lab
+        </a>
+        <div className={styles.navbarLinks}>
+          <a
+            className={styles.buttons}
+            href="https://ucsb-ocean-report.herokuapp.com/"
+          >
+            UCSB OCEAN REPORT
+          </a>
+          <a
+            className={styles.buttons}
+            href="https://ucsbcoastlab.org/diversity-equity-inclusion/"
+          >
+            DIVERSITY, EQUITY & INCLUSION
+          </a>
+          <a className={styles.buttons} href="https://ucsbcoastlab.org/buoy/">
+            UCSB_BUOY@CAMPUS POINT
+          </a>
+          <a
+            className={styles.buttons}
+            href="https://ucsbcoastlab.org/join-the-lab/"
+          >
+            OPPORTUNITIES
+          </a>
+          <a
+            className={styles.buttons}
+            href="https://ucsbcoastlab.org/rv-resonance/"
+          >
+            R/V RESONANCE
+          </a>
+          <a
+            className={styles.buttons}
+            href="https://ucsbcoastlab.org/callinectes/"
+          >
+            CALLINECTES
+          </a>
+          <a className={styles.buttons} href="https://ucsbcoastlab.org/people/">
+            PEOPLE
           </a>
         </div>
       </div>
+      <div className={styles.navbarBox}></div>
+      <div className={styles.leftbar}>
+        <h1 className={styles.title}>UCSB Ocean Report</h1>
+        <div className={styles.titleBox}></div>
+      </div>
+      <div className={styles.sidebar}></div>
       <Report />
     </div>
   );
