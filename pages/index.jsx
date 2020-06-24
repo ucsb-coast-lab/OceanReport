789fe99ca181acc4e236bf032cd6a6c62c022304
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-06-23 21:19:46 -0700

    sv -completed initial styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Report from "../components/report.jsx";
+import styles from "../styles/style.module.css";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>UCSB Ocean Report</h1>
+    <div className={styles.page}>
+      <div className={styles.navbar}>
+        <h1 className={styles.title}>UCSB Ocean Report</h1>
+        <div className={styles.homeButton}>
+          <a className={styles.buttons} href="https://ucsbcoastlab.org/">
+            Home
+          </a>
+        </div>
+      </div>
       <Report />
     </div>
   );
