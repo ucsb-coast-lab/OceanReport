627c2cbebfa5868db7ddd3940762d666248eddb7
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-07-02 12:57:20 -0700

    sv -cleaned up styling and added temp and basic graphs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,62 +1,282 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import fetch from "isomorphic-unfetch";
 import Report from "../components/report.jsx";
+import Graphs from "../components/graphs.jsx";
 import styles from "../styles/style.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDay } from "@fortawesome/free-solid-svg-icons";
 
 export default function HomePage() {
+  const [date, setDate] = useState("");
+  const [wave, setWave] = useState("");
+  const [wind, setWind] = useState("");
+  const [temp, setTemp] = useState("");
+  const [tide, setTide] = useState("");
+  const [hi, setHi] = useState("");
+  const [lo, setLo] = useState("");
+
+  const update = () => {
+    setWindWave();
+    setTempData();
+    setTideData();
+  };
+
+  function round(number, precision) {
+    var shift = function (number, exponent) {
+      var numArray = ("" + number).split("e");
+      return +(
+        numArray[0] +
+        "e" +
+        (numArray[1] ? +numArray[1] + exponent : exponent)
+      );
+    };
+    return shift(Math.round(shift(number, +precision)), -precision);
+  }
+
+  function timeConv(time) {
+    if (time.substring(0, 2) === "00") {
+      time = "12" + time.substring(2, 5);
+    } else {
+      time = (parseInt(time.substring(0, 2)) % 12) + time.substring(2, 5);
+    }
+    return time;
+  }
+
+  const setWindWave = async () => {
+    var url =
+      "https://api.sofarocean.com/api/latest-data?spotterId=SPOT-0186&limit=0&includeWindData=true";
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { token: process.env.SPOT_TOKEN },
+    });
+    const data = await response.json();
+
+    let currDate = "";
+    const dayOfWeek = new Date().getDay();
+    let weekday = isNaN(dayOfWeek)
+      ? null
+      : [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ][dayOfWeek];
+    const m = new Date().getMonth();
+    let month = isNaN(m)
+      ? null
+      : [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ][m];
+    const d = new Date(data.data.waves[1].timestamp.substring(0, 10)).getDate();
+    let time12 = new Date(data.data.waves[1].timestamp);
+    currDate +=
+      weekday +
+      ", " +
+      month +
+      " " +
+      (d + 1) +
+      " at " +
+      timeConv(time12.toString().substring(16, 21));
+    setDate(currDate);
+
+    let wave =
+      round(data.data.waves[1].significantWaveHeight / 0.3048, 1) +
+      " ft @ " +
+      parseInt(data.data.waves[1].peakPeriod) +
+      " secs from " +
+      round(data.data.waves[1].peakDirection, 0) +
+      "º";
+    setWave(wave);
+
+    let wind;
+    if (data.data.wind[1].speed < 2) {
+      wind = "Calm";
+    } else {
+      let dir = "north";
+      let theta = data.data.wind[1].direction;
+      if (theta >= 45 && theta < 135) {
+        dir = "east";
+      } else if (theta >= 135 && theta < 225) {
+        dir = "south";
+      } else if (theta >= 225 && theta < 315) {
+        dir = "west";
+      }
+      wind =
+        "From the " + dir + " at " + parseInt(data.data.wind[1].speed) + " kts";
+    }
+    setWind(wind);
+  };
+
+  const setTempData = async () => {
+    var url =
+      "https://cors-anywhere.herokuapp.com/" +
+      "https://erddap.sccoos.org/erddap/tabledap/autoss.json" +
+      "?time%2Ctemperature&station=%22stearns_wharf" +
+      "%22&time%3E=2020-07-01T07%3A00%3A00Z&time%3C2020-07-03T06%3A59%3A59Z&orderByMax(%22time%22)";
+    const response = await fetch(url, { method: "GET" });
+    const data = await response.json();
+    let far = data.table.rows[0][1] * (9.0 / 5.0) + 32;
+    let temp = "Water Temp: " + round(far, 1) + " ºF";
+    setTemp(temp);
+  };
+
+  const setTideData = async () => {
+    const current = new Date();
+    let year = current.getFullYear();
+    let month = current.getMonth() + 1;
+    let m = "00" + month;
+    m = m.substr(m.length - 2);
+    let day = current.getDate();
+    let d = "00" + day;
+    d = d.substr(d.length - 2);
+    const next = new Date(current);
+    next.setDate(next.getDate() + 1);
+    let year2 = next.getFullYear();
+    let month2 = next.getMonth() + 1;
+    let m2 = "00" + month2;
+    m2 = m2.substr(m2.length - 2);
+    let day2 = next.getDate();
+    let d2 = "00" + day2;
+    d2 = d2.substr(d2.length - 2);
+
+    let today = year.toString() + m + d;
+    let tomorrow = year2.toString() + m2 + d2;
+
+    var url =
+      "https://tidesandcurrents.noaa.gov/api/datagetter?" +
+      "station=9411340" +
+      "&product=predictions" +
+      "&datum=mllw" +
+      "&units=english" +
+      "&time_zone=gmt" +
+      "&application=UCSB" +
+      "&format=json" +
+      "&interval=hilo" +
+      "&begin_date=" +
+      today +
+      "&end_date=" +
+      tomorrow;
+    const response = await fetch(url, { method: "GET" });
+    const data = await response.json();
+
+    let t_pred = {};
+    let n_pred = 0;
+    let curr_time = current.getHours() * 60 + current.getMinutes();
+    data.predictions.map((prediction) => {
+      let time = new Date(prediction.t + " UTC");
+      let p_time =
+        parseInt(time.toString().substring(16, 18), 10) * 60 +
+        parseInt(time.toString().substring(19, 21));
+
+      if (
+        ((parseInt(time.toString().substring(8, 11)) === day &&
+          p_time > curr_time) ||
+          parseInt(time.toString().substring(8, 11)) === day2) &&
+        n_pred < 2
+      ) {
+        t_pred[n_pred] = prediction;
+        n_pred++;
+      }
+    });
+
+    let first, second;
+    if (t_pred[0].type === "H") {
+      first = "HI ";
+      second = "LO ";
+    } else {
+      first = "LO: ";
+      second = "HI: ";
+    }
+    let height = round(parseFloat(t_pred[0].v), 1);
+    let height2 = round(parseFloat(t_pred[1].v), 1);
+    let time1 = new Date(t_pred[0].t + " UTC");
+    let time2 = new Date(t_pred[1].t + " UTC");
+    let t1 = timeConv(time1.toString().substring(16, 21));
+    let t2 = timeConv(time2.toString().substring(16, 21));
+    first += height + " ft @ " + t1;
+    second += height2 + " ft @ " + t2;
+    setHi(first);
+    setLo(second);
+
+    url =
+      "https://tidesandcurrents.noaa.gov/api/datagetter?" +
+      "station=9411340" +
+      "&product=water_level" +
+      "&datum=mllw" +
+      "&units=english" +
+      "&time_zone=gmt" +
+      "&application=UCSB" +
+      "&format=json" +
+      "&date=latest";
+    const response2 = await fetch(url, { method: "GET" });
+    const data2 = await response2.json();
+
+    let currTide =
+      "Tide: " + round(parseFloat(data2.data[0].v), 1) + " ft and ";
+    if (data2.data[0].v < t_pred[0].v) {
+      currTide += "rising";
+    } else {
+      currTide += "falling";
+    }
+
+    setTide(currTide);
+  };
+
+  if (date === "") {
+    setDate(" ");
+    update();
+  }
+
   return (
     <div className={styles.page}>
       <div className={styles.navbar}>
-        <a className={styles.navbarTitle} href="https://ucsbcoastlab.org/">
-          UCSB COAST Lab
-        </a>
-        <div className={styles.navbarLinks}>
-          <a className={styles.buttons} href="https://ucsbcoastlab.org/people/">
-            PEOPLE
+        <div className={styles.dateTitleBox}>
+          <a className={styles.title} href="https://ucsbcoastlab.org/">
+            UCSB COAST Lab Ocean Report
           </a>
-          <a
-            className={styles.buttons}
-            href="https://ucsbcoastlab.org/callinectes/"
-          >
-            CALLINECTES
-          </a>
-          <a
-            className={styles.buttons}
-            href="https://ucsbcoastlab.org/rv-resonance/"
-          >
-            R/V RESONANCE
-          </a>
-          <a
-            className={styles.buttons}
-            href="https://ucsbcoastlab.org/join-the-lab/"
-          >
-            OPPORTUNITIES
-          </a>
-          <a className={styles.buttons} href="https://ucsbcoastlab.org/buoy/">
-            UCSB_BUOY@CAMPUS POINT
-          </a>
-          <a
-            className={styles.buttons}
-            href="https://ucsbcoastlab.org/diversity-equity-inclusion/"
-          >
-            DIVERSITY, EQUITY & INCLUSION
-          </a>
-          <a
-            className={styles.buttons}
-            href="https://ucsb-ocean-report.herokuapp.com/"
-          >
-            {" "}
-            UCSB OCEAN REPORT{" "}
-          </a>
+          <div className={styles.date}>
+            <FontAwesomeIcon
+              className={styles.iconsWhite}
+              icon={faCalendarDay}
+            />
+            <p>{date}</p>
+          </div>
+        </div>
+        <div className={styles.report}>
+          <Report
+            date={date}
+            wave={wave}
+            wind={wind}
+            temp={temp}
+            tide={tide}
+            hi={hi}
+            lo={lo}
+          />
         </div>
       </div>
       <div className={styles.navbarBox}></div>
+
       <div className={styles.content}>
-        <div className={styles.leftbar}>
-          <div className={styles.titleBox}></div>
+        <div className={styles.left}></div>
+        <div className={styles.right}>
+          <div className={styles.graphs}>
+            <Graphs />
+          </div>
         </div>
-        <h1 className={styles.title}>UCSB Ocean Report</h1>
-        <div className={styles.sidebar}></div>
-        <Report />
       </div>
     </div>
   );
