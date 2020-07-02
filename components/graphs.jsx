64b50dfed350bf431960d9b8551f59e4a79885ea
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-07-02 12:57:20 -0700

    sv -cleaned up styling and added temp and basic graphs

---
+++ components/graphs.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import styles from "../styles/style.module.css";
+import ChartistGraph from "react-chartist";
+
+export default function Graphs() {
+  var WaveData = {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    series: [[3.5, 2.2, 2.7, 1.3, 0.6]],
+  };
+
+  var WindData = {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    series: [[4, 3, 2, 1, 0]],
+  };
+
+  var TempData = {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    series: [[4, 3, 2, 1, 0]],
+  };
+
+  var TideData = {
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+    series: [[4, 3, 2, 1, 0]],
+  };
+
+  var options = {
+    width: "500px",
+    height: "300px",
+  };
+
+  let type = "Line";
+
+  return (
+    <div>
+      <h1>2-Day Wave Height Chart</h1>
+      <ChartistGraph data={WaveData} options={options} type={type} />
+      <h1>2-Day Wind Chart</h1>
+      <ChartistGraph data={WindData} options={options} type={type} />
+      <h1>2-Day Water Temp Chart</h1>
+      <ChartistGraph data={TempData} options={options} type={type} />
+      <h1>2-Day Tide Chart</h1>
+      <ChartistGraph data={TideData} options={options} type={type} />
+    </div>
+  );
+}
